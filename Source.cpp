5e0bee46731c899f5513d80472f34056d7ddc597
User <User@DENIS_PC>
2022-02-08 13:23:27 +0300

    Добавьте файлы проекта.

---
+++ Source.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#define inf INT_MAX
+
+
+using namespace std;
+
+void Dijkstra(bool* v, int* d, int** arr, int N) {
+
+	int ind = inf, mini = inf;
+	do {
+		ind = inf, mini = inf;
+		for (int i = 0; i < N; i++) {
+			if (!v[i] && d[i] < mini) {
+				ind = i;
+				mini = d[i];
+			}
+		}
+		if (ind != inf) {
+			for (int i = 0; i < N; i++) {
+				if (arr[ind][i] != 0 && (d[i] > d[ind] + arr[ind][i])) {
+					d[i] = d[ind] + arr[ind][i];
+				}
+			}
+			v[ind] = 1;
+		}
+		else return;
+	} while (1);
+}
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	//ѕосещенные вершины
+	bool* v = new bool[N];
+	for (int i = 0; i < N; i++) {
+		v[i] = 0;
+	}
+
+	//—оздание таблицы смежности, инициализаци€ нул€ми, заполнение
+	int** arr = new int* [N];
+	for (int i = 0; i < N; i++) {
+		arr[i] = new int[N];
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			arr[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		a--; b--;
+		arr[a][b] = c;
+		arr[b][a] = c;
+	}
+
+	//¬ычисление длинны до ребер, вывод
+	int *d = new int[N];
+	d[0] = 0;
+	for (int i = 1; i < N; i++) {
+		d[i] = inf;
+	}
+	Dijkstra(v, d, arr, N);
+	cout << endl << endl;
+	for (int i = 0; i < N; i++) {
+		cout << i + 1 << ' ' << d[i] << endl;
+	}
+
+
+	//”даление динамических массивов
+	delete[] v;
+	delete[] d;
+	for (int i = 0; i < N; i++) {
+		delete[] arr[i];
+	}
+	return 0;
+}
